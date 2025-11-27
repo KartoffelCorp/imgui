061b8d0d71f635c51bd178cdc3c2618bb2a0b82e
Cedric Neukirchen <cedric@saltypandastudios.com>
2025-11-28 00:57:50 +0100

    Modified premake5.lua file.

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,6 @@ project "ImGui"
     kind "StaticLib"
     language "C"
     cppdialect "C++20"
-    warnings "Off"
 
     targetdir ("Binaries/" .. outputdir)
     objdir("Intermediate/" .. outputdir)
@@ -19,12 +18,7 @@ project "ImGui"
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-        "imgui_demo.cpp",
-        "backends/imgui_impl_glfw.h",
-        "backends/imgui_impl_glfw.cpp",
-        "backends/imgui_impl_opengl3.h",
-        "backends/imgui_impl_opengl3.cpp",
-        "backends/imgui_impl_opengl3_loader.h"
+        "imgui_demo.cpp"
     }
 
     includedirs
@@ -33,13 +27,24 @@ project "ImGui"
         "%{IncludeDir.GLFW}"
     }
 
+    -- Platform Windows
     filter "system:windows"
         systemversion "latest"
+        files
+        {
+            "backends/imgui_impl_glfw.h",
+            "backends/imgui_impl_glfw.cpp",
+            "backends/imgui_impl_opengl3.h",
+            "backends/imgui_impl_opengl3.cpp",
+            "backends/imgui_impl_opengl3_loader.h"
+        }
 
+    -- Config Debug
     filter "configurations:Debug"
         runtime "Debug"
         symbols "On"
 
+    -- Config Release
     filter "configurations:Release"
         runtime "Release"
         optimize "On"
